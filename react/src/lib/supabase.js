579402a9775f99533914a31827e48c8d153561aa
josselynturcios13-jjt <jeymiturcios13@gmail.com>
2026-06-02 22:12:13 -0600

    feat: configuracion de supabase y .env
    En este paso lo que hicimos fue conectar la BD de supabase y poner las credenciales en .env de modo seguro y no se exponen publicamente

---
+++ react/src/lib/supabase.js
@@ -0,0 +1,6 @@
+import {createcliente} from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://hathtchxxewchkeqztsk.supabase.co'
+const supabaseKey = 'sb_publishable_dMnLes9J0pm15ktK570BCw_RH56nkX0' 
+
+export const supabase = createcliente(supabaseUrl, supabaseKey)
