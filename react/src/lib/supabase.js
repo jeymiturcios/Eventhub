150515bf79f19e47d622a5314eb9c88a15feda30
Chris Duarte <147928918+ChrisD04@users.noreply.github.com>
2026-06-04 21:50:01 -0600

    Update supabase.js
    Cambio en la conexion de la BD

--- react/src/lib/supabase.js
+++ react/src/lib/supabase.js
@@ -1,7 +1,6 @@
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://hathtchxxewchkeqztsk.supabase.co'
 const supabaseKey = 'sb_publishable_dMnLes9J0pm15ktK570BCw_RH56nkX0'
-const supabaseKey = "TU-ANON-KEY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
